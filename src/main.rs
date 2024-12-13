b06e39360e0eac8daca42be84ac96300d047c3b6
Alleexxi <66967403+Alleexxi@users.noreply.github.com>
2024-12-13 20:32:41 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,37 @@
 use eframe::egui;
 use goldberg::goldberg_stmts;
-use tsar_client::{Client, ClientOptions};
+use tsar_sdk::{AuthParams, Client, ClientParams};
 
 fn main() -> Result<(), eframe::Error> {
     goldberg_stmts! {{
-        let options = ClientOptions {
-            app_id: "d45750ee-7c2c-4453-9a90-397007bc1b9d".to_string(),
-            client_key: "MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAElfwlA3IWW8+tI4+T0HNYA0ZmaTQEqBrEbygPgoaAvsK68KRvVXJN/IXOthd3ulxv6LCPqdRewAsG7srEmwAzhA==".to_string(),
-            debug_print: true
+        let options = ClientParams {
+            app_id: "f911842b-5b3d-4c59-b5d1-4adb8f71557b".to_string(),
+            client_key: "MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEvJrwPvdeDUcV8Qr02tzgFrp+8qfCV/vG1HcQJYYV8u5vYUfGABMAYT0qOQltXEX9DTcB2fzLfwQnl7yiAaNruQ==".to_string(),
         };
 
-        let client = Client::new(options).expect("Authentication failed.");
+        let client = Client::create(options).expect("Authentication failed.");
+        
+        let mut user_result = client.authenticate(AuthParams::default());
+        while user_result.is_err() {
+            println!("Attempting to check if user is valid...");
+            std::thread::sleep(std::time::Duration::from_secs(3));
+
+            user_result = client.authenticate(AuthParams {
+                open_browser: false,
+            });
+        }
+
+        let user = user_result.unwrap();
+        println!("User authorized. {:#?}", user);
 
         eframe::run_native(
             "TSAR Rust Demo",
             eframe::NativeOptions::default(),
             Box::new(|_| {
                 Box::new(App {
-                    username: client.subscription.user.username.unwrap(),
-                    id: client.subscription.user.id,
-                    hwid: client.hwid,
+                    username: user.name.unwrap_or_else(|| "Unknown".to_string()),
+                    id: user.id,
+                    hwid: Client::get_hwid().unwrap_or_else(|_| "Unknown HWID".to_string()),
                 })
             }),
         )
